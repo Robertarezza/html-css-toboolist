051f8b91bba12bda2d6625353143e8ece81bdef3
Robertarezza <roberta.rezza@gmail.com>
2024-04-19 15:19:38 +0200

    add: milestone 1.1

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
     - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
 # MILESTONE 1
-- [ ] Stampare all'interno di una lista, un item per ogni todo.
+- [x] Stampare all'interno di una lista, un item per ogni todo.
 - [] Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
 # MILESTONE 2
--- js/script.js
+++ js/script.js
@@ -3,8 +3,40 @@ const {createApp} = Vue
 createApp ( {
 data() {
      return {
-    
-        
+todoList: [
+     {
+        text: "Quarterly Newsletter",
+        done: true,
+    },
+    {
+        text: "Mobile app luaunch",
+        done: true,
+    },
+     {
+        text: "Interview Jhon H",
+        done: false,
+    },
+    {
+        text: " Summit update to mobile storefronts",
+        done: true,
+    },
+     {
+        text: "Shedule meeting with Alexr",
+        done: false,
+    },
+     {
+        text: "Homepage refresh",
+        done: true,
+    },
+     {
+        text: "Onboard new Sales team members",
+        done: false,
+    },
+   {
+        text: "Review editorial calendar",
+        done: true,
+    },
+]
     }
  },
  created() {
